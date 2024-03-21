23be50c1316e28b19e0e056b1a1e3195c1e49e91
carry0987 <carry0987@users.noreply.github.com>
2024-03-21 08:38:35 +0800

    Add base64UrlDecode method

--- src/Utils/HTTPUtil.php
+++ src/Utils/HTTPUtil.php
@@ -7,4 +7,9 @@ class HTTPUtil
     {
         return rtrim(strtr(base64_encode($data), '+/', '-_'), '=');
     }
+
+    public static function base64UrlDecode(string $data)
+    {
+        return base64_decode(str_pad(strtr($data, '-_', '+/'), strlen($data) % 4, '=', STR_PAD_RIGHT));
+    }
 }
